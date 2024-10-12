72ab16c01d6fff46cadeb6366af8f807a4b0a861
AaronCruzIT4tocreativo <aaron.it.4tocreativo@gmail.com>
2024-10-12 17:06:38 -0600

    Refactor Custom Driver elements to element_names

--- src/custom_driver.py
+++ src/custom_driver.py
@@ -2,15 +2,15 @@ from dataclasses import dataclass, field
 
 @dataclass
 class CustomDriver:
-    elements: list = field(default_factory=list)
+    element_names: list = field(default_factory=list)
     values: list = field(default_factory=list)
 
-    def set_input_elements(self, elements: list):
-        self.elements = elements
+    def set_input_element_names(self, element_names: list):
+        self.element_names = element_names
 
     def set_input_values(self, values: list):
         self.values = values
 
     def find_input_element(self, element: str):
-        if element not in self.elements: 
+        if element not in self.element_names: 
             return "The input element is not found"
