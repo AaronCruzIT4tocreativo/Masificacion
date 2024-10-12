aac8cc1ec90084a82df41e9b8e06accef21c5f78
AaronCruzIT4tocreativo <aaron.it.4tocreativo@gmail.com>
2024-10-12 15:38:31 -0600

    Add Custom Driver Test

---
+++ tests/test_custom_driver.py
@@ -0,0 +1,13 @@
+import pytest
+from custom_driver import CustomDriver
+
+@pytest.fixture
+def custom_driver():
+    cd = CustomDriver()
+    cd.set_input_elements(["input0", "input1", "input2"])
+    cd.set_input_values(["value0", "value1", "value2"])
+    return cd
+
+def test_find_element(custom_driver):
+    expected_input = "input3"
+    assert "The input element is not found" == custom_driver.find_input_element(expected_input)
