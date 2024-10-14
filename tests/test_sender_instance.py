4683362ec6f5193bf061f63efc3f3f2dc40ebdd7
AaronCruzIT4tocreativo <aaron.it.4tocreativo@gmail.com>
2024-10-14 11:04:59 -0600

    Refactor Sender Instance Test

--- tests/test_sender_instance.py
+++ tests/test_sender_instance.py
@@ -4,12 +4,17 @@ from sender import Sender
 
 @pytest.fixture
 def sender_instance():
-    si = SenderInstance()
+    si = SenderInstance(sender = sender)
     return si
 
 @pytest.fixture
 def sender():
     s = Sender()
+    values = [
+        ["654", "hola"],
+        ["123", "adios"]
+    ]
+    s.add_values_to_queue(values)
     return s
 
 def test_play(sender_instance):
@@ -24,13 +29,13 @@ def test_stop(sender_instance, sender):
     sender_instance.stop()
     assert sender_instance.events.play.is_set() == False
     assert len(sender.queue) == 0
-    # assert sender_instance.events.stop.is_set() == False
 
 def test_restart(sender_instance, sender):
     values = [
-        []
+        ["997", "Hi from 997"],
+        ["998", "Hi from 998"],
+        ["999", "Hi from 999"]
     ]
-    sender_instance.restart()
+    sender_instance.restart(values)
     assert sender_instance.events.play.is_set() == True
-    assert len(sender.queue) == 0
-    # assert sender_instance.events.restart.is_set() == False
+    assert len(sender.queue) == 3
